414c2e1f24009a0a8f3d8ffe910b1a940988e8b4
DeviRajendra <38057669+DeviRajendra@users.noreply.github.com>
2018-07-09 09:15:46 +0530

    second

--- modal.js
+++ modal.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
 	$(".launch").click(function(){
 		$(".modal").show();
 		$("body").css("background-color","grey");
-	})
+	});
 	$(".close").click(function(){
 		$(".modal").hide();
 		$("body").css("background-color","");
-	})
+	});
 })
