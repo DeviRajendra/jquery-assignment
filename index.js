186fbd4b1e3d3f7f03d50e626f28c550c95e85dd
DeviRajendra <38057669+DeviRajendra@users.noreply.github.com>
2018-07-09 14:26:31 +0530

    Third

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ $(document).ready(function(){
 
 	$(".btn1").click(function(){
 		$(".p1").toggle();
-		$(".btn1").css({"color":"blue", "text-decoration":"underline"});
+		$(".btn1").css({"color":"blue", "text-decoration":"underline","background-color":"skyblue"});
 		$(".p2").hide();
 		$(".p3").hide();
-		$(".btn2").css({"color":"", "text-decoration":"none"});
-		$(".btn3").css({"color":"", "text-decoration":"none"});
+		$(".btn2").css({"color":"", "text-decoration":"none","background-color":""});
+		$(".btn3").css({"color":"", "text-decoration":"none","background-color":""});
 		
 	})
 
 	$(".btn2").click(function(){
 		$(".p2").toggle();
-		$(".btn2").css({"color":"blue", "text-decoration":"underline"});
+		$(".btn2").css({"color":"blue", "text-decoration":"underline","background-color":"skyblue"});
 		$(".p1").hide();
 		$(".p3").hide();
-		$(".btn1").css({"color":"", "text-decoration":"none"});
-		$(".btn3").css({"color":"", "text-decoration":"none"});
+		$(".btn1").css({"color":"", "text-decoration":"none","background-color":""});
+		$(".btn3").css({"color":"", "text-decoration":"none","background-color":""});
 	})
 	$(".btn3").click(function(){
 		$(".p3").toggle();
-		$(".btn3").css({"color":"blue", "text-decoration":"underline"});
+		$(".btn3").css({"color":"blue", "text-decoration":"underline","background-color":"skyblue"});
 		$(".p1").hide();
 		$(".p2").hide();
-		$(".btn2").css({"color":"", "text-decoration":"none"});
-		$(".btn1").css({"color":"", "text-decoration":"none"});
+		$(".btn2").css({"color":"", "text-decoration":"none","background-color":""});
+		$(".btn1").css({"color":"", "text-decoration":"none","background-color":""});
 	})
 
 
